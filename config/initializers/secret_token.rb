aa1e47c09149ac9d23fe32f58d51addabc8eb742
Alex Tandrau <alexandru@tandrau.com>
2012-10-03 20:23:01 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrippingOctoNinja::Application.config.secret_token = '02d7c9f93e39992b13f3238a6c8a631b81ac0b303fdf1fe4215e516a0b8cf0f67d0fc61fd2c5ad7880249329e8223f927682d020b1c579e4f35f7829222210f0'
