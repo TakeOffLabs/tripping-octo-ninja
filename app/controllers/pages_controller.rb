3152d5e0b4fa141b55ebba3ac13ad09883d15d7a
Alex Tandrau <alexandru@tandrau.com>
2012-10-03 20:57:19 +0200

    printing pdf 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,13 @@
 class PagesController < ApplicationController
+
+  def print
+    @message = params[:message]
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "print", disposition: "attachment"
+      end
+    end
+  end
 end
